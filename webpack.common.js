b59f63357c5fad3b8df40e813f66a92c582eb1a0
이우진 <dldndldms@gmail.com>
2025-07-05 09:55:15 +0900

    feat: 웹팩 설정 파일 추가 (common, dev, prod 환경분리)

---
+++ webpack.common.js
@@ -0,0 +1,70 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: {
+      import: "./src/index.js",
+      dependOn: "shared",
+    },
+    print: {
+      import: "./src/print.js",
+      dependOn: "shared",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "TimerTodoList",
+    }),
+  ],
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    globalObject: "this",
+    library: {
+      name: "TimerTodoList",
+      type: "umd",
+    },
+    clean: true,
+  },
+  stats: "normal",
+  optimization: {
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
+    removeAvailableModules: false,
+    removeEmptyChunks: false,
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"], // 체인은 역순으로 실행
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i, // 이미지 파일 처리
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i, // 폰트 파일 처리
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@components": path.resolve(__dirname, "src/components"),
+    },
+  },
+};
