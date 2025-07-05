ed053168baa40d9559dbc659fdd424d31b18ae26
이우진 <dldndldms@gmail.com>
2025-07-05 10:25:26 +0900

    feat: 기본 컴포넌트 클래스 추가 - React 컴포넌트 구조를 위한 초기화, 업데이트, 언마운트 및 렌더링 메서드 포함

---
+++ src/components/common/Component.js
@@ -0,0 +1,43 @@
+/**
+ * 기본 컴포넌트. 모든 컴포넌트는 다음 클래스를 확장하여 사용합니다.
+ * init, update, unmount, render등 React 컴포넌트의 구조를 생각해서 사용할 계획입니다.
+ */
+export class Component {
+  constructor(props = {}) {
+    this.props = props;
+    this.el = this.createElement();
+    this.init();
+  }
+
+  init() {
+    this.render();
+    if (typeof this.componentDidMount === "function") {
+      this.componentDidMount();
+    }
+  }
+
+  update(newProps = {}) {
+    this.props = { ...this.props, ...newProps };
+    this.render();
+    if (typeof this.componentDidUpdate === "function") {
+      this.componentDidUpdate();
+    }
+  }
+
+  unmount() {
+    if (typeof this.componentWillUnmount === "function") {
+      this.componentWillUnmount();
+    }
+    if (this.el && this.el.parentNode) {
+      this.el.parentNode.removeChild(this.el);
+    }
+  }
+
+  createElement() {
+    return document.createElement("div"); // 기본값
+  }
+
+  render() {
+    // override 필수
+  }
+}
