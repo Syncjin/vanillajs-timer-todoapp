3c5591fde17df9cd505cdb90e0c32cadaea6f4c8
이우진 <dldndldms@gmail.com>
2025-07-07 14:36:44 +0900

    feat: Button 컴포넌트에 children 속성 추가, chech svg 파일 추가

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -1,14 +1,14 @@
 import { Component } from "@components/core/Component";
-
+import { renderDom } from "@components/core/vdom";
 export class Button extends Component {
   createElement() {
     return document.createElement("button");
   }
 
   render() {
-    const { label = "Button", className = "", disabled = false, onClick } = this.props;
+    const { label = "Button", className = "", disabled = false, onClick, children = null } = this.props;
 
-    this.el.textContent = label;
+    this.el.innerHTML = "";
     this.el.className = className;
     this.el.disabled = disabled;
 
@@ -16,6 +16,14 @@ export class Button extends Component {
     this.el.onclick = null;
     if (typeof onClick === "function") {
       this.el.addEventListener("click", onClick);
+    }
+
+    if (Array.isArray(children)) {
+      children.map(renderDom).forEach((child) => this.el.appendChild(child));
+    } else if (typeof children === "object" && children !== null) {
+      this.el.appendChild(renderDom(children));
+    } else {
+      this.el.textContent = label;
     }
   }
 }
