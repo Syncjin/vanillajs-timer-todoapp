6030a78ec9363373667c53ea03c6fa3f1fa9a7f3
이우진 <dldndldms@gmail.com>
2025-07-08 13:52:42 +0900

    feat: production 모드 console.log 제거

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,19 @@
 const { merge } = require("webpack-merge");
+const TerserPlugin = require("terser-webpack-plugin");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true, // console.log 제거
+            drop_debugger: true, // debugger 제거
+          },
+        },
+      }),
+    ],
+  },
 });
